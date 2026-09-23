5f89f7054f9d8b243c07b7ca01131c72ee0f83a9
floresgregorio <floresgregorio@hotmail.com>
2026-09-23 16:00:52 -0600

    dicomconvert 0.1.20

--- Formula/dicomconvert.rb
+++ Formula/dicomconvert.rb
@@ -1,29 +1,29 @@
 class Dicomconvert < Formula
   desc "Convert the first frame of DICOM files to images"
   homepage "https://github.com/aniachi/homebrew-dicomconvert"
-  version "0.1.14"
+  version "0.1.20"
   license "Beerware"
 
   on_macos do
     depends_on macos: :ventura
     on_arm do
-      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.14/dicomconvert-v0.1.14-aarch64-apple-darwin.tar.gz"
-      sha256 "cd9e366fc4316a69e7ba8645dc8b7dcb88fe5575a1677b9b5d2d654a428a0dd4"
+      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.20/dicomconvert-v0.1.20-aarch64-apple-darwin.tar.gz"
+      sha256 "2df9fc6e163cbb68e33673babdf879c8ae61afd00c7824f57810a18a22421bdd"
     end
     on_intel do
-      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.14/dicomconvert-v0.1.14-x86_64-apple-darwin.tar.gz"
-      sha256 "2f938a1ddc3d9c88921977aa6b06d2dee41e9e73ef3515bdaffadf27812f1902"
+      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.20/dicomconvert-v0.1.20-x86_64-apple-darwin.tar.gz"
+      sha256 "438678c69f51d7ea9005424d5018c61c112ec10eb202acf60b9b534db02a8996"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.14/dicomconvert-v0.1.14-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "577cccf1a59cb76e53a9b8745c7d1c152a89136cc394ef681eeebd87e365506f"
+      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.20/dicomconvert-v0.1.20-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "4d934aa994489882adac7374e897ab82a819f741d321efcdf48d3e45dc9cfb86"
     end
     on_arm do
-      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.14/dicomconvert-v0.1.14-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "7bff88ae189df9359d2865e9a0926f37b71aa44784bcb571da44d6c05fb30b48"
+      url "https://github.com/aniachi/homebrew-dicomconvert/releases/download/v0.1.20/dicomconvert-v0.1.20-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "3e0ad7bc504fe71dd4df5ac3c483f98986c2e70d02fe72672c246df865259aa1"
     end
   end
 
